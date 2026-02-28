cceb18d61d0be0f7f081a1b15abd3310870dd9ef
jecmui <jessicamui90@gmail.com>
2026-02-28 17:56:06 -0500

    small change to logo alt text

--- app/components/login/login.tsx
+++ app/components/login/login.tsx
@@ -5,7 +5,7 @@ function Login() {
         <div className="flex flex-col max-w-lg mx-auto">
             <img 
                 src="public/bullet-logo-full.png"
-                alt="The full bullet website logo. It features the handwritten text 'bullet' in all lowercase with two stars, one underneath the letters and one to the top right."
+                alt="The full bullet website logo. It features the handwritten text 'bullet' in all lowercase with serifs, two stars, one underneath the letters and one to the top right."
                 className="w-lg flex mx-auto"
             />
             <form className="">
