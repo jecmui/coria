53eb33222d8c4c5ba7109a32430d4a470fad9e56
jecmui <jessicamui90@gmail.com>
2026-04-24 01:53:52 -0400

    oops cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,16 @@
 import type { Route } from "./+types/home";
-import Login from "~/components/auth/login";
 import Landing from "~/components/auth/landing";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "bullet" },
-    { name: "description", content: "Your very own digital bulletin board." },
-  ];
+    return [
+        { title: "bullet" },
+        {
+            name: "description",
+            content: "Your very own digital bulletin board.",
+        },
+    ];
 }
 
 export default function Home() {
-  return <Landing />;
+    return <Landing />;
 }
