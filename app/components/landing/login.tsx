8136d838a6478ea5ab4bf2fa45923a79deeeb820
jecmui <jessicamui90@gmail.com>
2026-04-23 22:47:36 -0400

    add new pw visibility to login + fix errors

--- app/components/landing/login.tsx
+++ app/components/landing/login.tsx
@@ -30,7 +30,7 @@ function Login() {
         const error2 = validatePassword(password);
         if (error1 || error2) {
             setEmailError(error1);
-            setEmailError(error2);
+            setPasswordError(error2);
         } else {
             // redirect
         }
@@ -89,19 +89,21 @@ function Login() {
                         className="absolute text-sm text-body duration-300 transform -translate-y-3 scale-75 top-4.5 z-10 origin-[0] start-3.5 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto text-(--color-primary-grey)">
                             {"Password"}
                     </label>
-                    <button
-                        type="button"
-                        onClick={() => setShowPassword(!showPassword)}
-                        className="absolute right-3 top-1/2 -translate-y-1/2 pr-1 py-2 text-(--color-primary-grey) hover:text-(--color-primary-grey-dark) cursor-pointer"
-                    >
-                        {showPassword ? <FaEyeSlash /> : <FaEye />}
-                    </button>
                 </div>
+
                 {passwordError && (
                     <div className="text-red-500 text-xs -mt-2">
                         {passwordError}
                     </div>
                 )}
+
+                <div className="mb-0 -mt-2">
+                    <input type="checkbox" id="show-password-checkbox" className="accent-(--color-primary-grey) hover:accent-(--color-primary-grey-dark)" onClick={() => setShowPassword(!showPassword)} value="Show password"/>
+                    <label htmlFor="show-password-checkbox" className="text-(--color-primary-grey) hover:text-(--color-primary-grey-dark) text-sm pl-1.5">
+                        Show password
+                    </label>
+                </div>
+
                 <input type="submit" value="Login" className="block bg-(--color-primary-blue) hover:bg-(--color-primary-blue-dark) text-white px-6 py-3 mt-6 rounded-2xl mx-auto w-full sm:w-auto cursor-pointer "/>
             </form>
             <div className="mt-6 text-(--color-primary-grey)">
